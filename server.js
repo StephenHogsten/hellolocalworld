58b57ebc6db72c05520dc1ab25d9b99ec6d4b4a9
HogDogTheGod <throweymcthrowerson@gmail.com>
2017-02-10 11:49:22 -0500

    enabled .env

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 for (var i=0,j; i<10; i++) {
   j = i*20;
   console.log('number ' + i);
   console.log(' other number ' + j);
+  console.log(process.env.MONGO_URI);
 }
+return 
